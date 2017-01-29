7ee1d910d5c8457d04faa518b372f9764b742873
Ben Godfrey <bcgodfrey91@gmail.com>
2017-01-28 23:06:32 -0700

    Add contact sheet

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,55 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Contact extends Component {
+class Contact extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      name: '',
+      email: '',
+      message: ''
+    };
+  }
+
+  nameField(e) {
+    this.setState({name: e.target.value});
+  }
+
+  emailField(e) {
+    this.setState({email: e.target.value});
+  }
+
+  messageField(e) {
+    this.setState({message: e.target.value});
+  }
+
+  enableSubmit() {
+    if (this.state.name === '' ||
+        this.state.email.indexOf('@') === -1 ||
+        this.state.message === '') {
+      return true;
+    }
+  }
+
   render() {
     return (
-      <div className="Contact">
-        <h1>Contact</h1>
-      </div>
-    );
+      <section id="contact">
+        <h1 className='contact-header'>Contact me</h1>
+        <form action="https://formspree.io/bcgodfrey91@gmail.com"
+          method="POST">
+          <input onKeyUp={(e)=>this.nameField(e)} className='name'
+          placeholder='Your Name' type="text" name="name"/>
+          <input onKeyUp={(e)=>this.emailField(e)} className='email'
+          placeholder='Your Email' type="email" name="_replyto"/>
+          <textarea onKeyUp={(e)=>this.messageField(e)} className='message'
+          placeholder='Message' type='text' name="message"></textarea>
+          <input className="submit" type="submit" value="Send"
+          disabled={this.enableSubmit()} />
+          <h1>{this.state.name}</h1>
+          <h1>{this.state.email}</h1>
+          <h1>{this.state.message}</h1>
+        </form>
+      </section>
+    )
   }
 }
 
