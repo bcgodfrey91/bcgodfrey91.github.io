30f422d469a270d1547f97656db4a6a659a3079c
Ben Godfrey <bcgodfrey91@gmail.com>
2017-02-28 23:09:15 -0700

    Add base level of mapping through projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,88 +3,34 @@ import '../styles/Projects.css';
 import npmLogo from '../images/npm-logo.png'
 import amptron from '../images/amptron.png'
 import shootTheBreeze from '../images/shoot-the-breeze.png'
-
-
+import helpers from './projectHelper'
 
 class Projects extends Component {
+
+  renderProjects() {
+    return helpers.projects.map(project => {
+      return (
+          <div className='project-container'>
+            <div className='project-card'>
+              <img src={project.img} alt='NPM logo' />
+              <h3 className='project-title'>{project.name}</h3>
+            </div>
+            <div className='project-info'>
+              <div className='project-technologies'>
+                <p>{project.technologies}</p>
+              </div>
+            </div>
+          </div>
+      )
+    })
+  }
+
+
   render() {
     return (
       <div className="Projects">
         <span><h1>Projects</h1></span>
-        <div className='project-container'>
-          <div className='project-card'>
-            <img src={npmLogo} alt='NPM logo' />
-            <h3 className='project-title'>Electron Audio Conversion</h3>
-          </div>
-          <div className='project-info'>
-            <ul className='project-technologies'>
-              <li>ES6</li>
-              <li>Node.js</li>
-              <li>Babel</li>
-            </ul>
-            <p className='project-purpose'>Something something something</p>
-          </div>
-        </div>
-
-        <div className='project-container'>
-          <div className='project-card'>
-            <p>img</p>
-            <h2 className='project-title'>Data Hunt</h2>
-          </div>
-          <div className='project-info'>
-            <ul className='project-technologies'>
-              <li>Auth0</li>
-              <li>React Natve</li>
-              <li>Redux</li>
-              <li>Immutable.js</li>
-            </ul>
-            <p className='project-purpose'>Something something something</p>
-          </div>
-        </div>
-
-        <div className='project-container'>
-          <div className='project-card amptron'>
-            <img src={amptron} alt='Amptron music player' />
-            <h2 className='project-title'>Amptron</h2>
-          </div>
-          <div className='project-info'>
-            <ul className='project-technologies'>
-              <li>HTML5</li>
-              <li>Vue</li>
-              <li>Electron</li>
-              <li>Node.js</li>
-            </ul>
-            <p className='project-purpose'>Something something something</p>
-          </div>
-        </div>
-
-        <div className='project-container'>
-          <div className='project-card stb'>
-            <img src={shootTheBreeze} alt='Shoot The Breeze chatroom' />
-            <h2 className='project-title'>Shoot The Breeze</h2>
-          </div>
-          <div className='project-info'>
-            <ul className='project-technologies'>
-              <li>React</li>
-              <li>Firebase</li>
-            </ul>
-            <p className='project-purpose'>Something something something</p>
-          </div>
-        </div>
-
-        <div className='project-container'>
-          <div className='project-card'>
-            <p>img</p>
-            <h2 className='project-title'>Lights Out</h2>
-          </div>
-          <div className='project-info'>
-            <ul className='project-technologies'>
-              <li>jQuery</li>
-            </ul>
-            <p className='project-purpose'>Something something something</p>
-          </div>
-        </div>
-
+        {this.renderProjects()}
       </div>
     );
   }
---
+++ src/components/projectHelper.js
@@ -0,0 +1,12 @@
+import npmLogo from '../images/npm-logo.png'
+
+export default {
+  projects:[
+    {
+      id: 1,
+      name: 'NPM Package',
+      img: npmLogo,
+      technologies: 'ES6, Node.js, and Babel'
+    },
+  ]
+}
