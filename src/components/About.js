b97e45b04c78342b7b0b082161fe50b7bbffe0c8
Ben Godfrey <bcgodfrey91@gmail.com>
2017-03-24 13:41:13 -0500

    Small touch ups to styling and wording in About

--- src/components/About.js
+++ src/components/About.js
@@ -15,7 +15,7 @@ class About extends Component {
         />
         <div className='about-content'>
           <p>
-            I'm a former Co-Founder turned Front-End Developer. I pride myself in writing code that is modern, reusable, and accessible. I enjoy creating projects that are both minimal and beautiful, allowing the user to find joy in the subtleties of the design. I thrive on the constant rush and frustration of exploring new, cutting edge languages and frameworks, as I continue down the path of becoming a better developer.
+            I'm a former Co-Founder turned Front-End Developer. I pride myself in writing code that is modern, reusable, and accessible. I enjoy creating projects that are both minimal and elegant, allowing the user to find joy in the subtleties of the design. I thrive on the constant rush and frustration of exploring new, cutting edge languages and frameworks, as I continue down the path of becoming a better developer.
           </p>
           <p>
             I currently reside in Dallas, Texas, but have an undying impulse to travel that keeps me moving around as often my bank account allows. When I'm not coding or traveling, you can find me around live music, managing rappers, attempting to get to the gym, or aimlessly roaming the internet.
